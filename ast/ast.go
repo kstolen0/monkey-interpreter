b44aebdd33462c0afc89998bd65ee50bbb824ea9
Kristian Stolen <kstolen0@gmail.com>
2024-02-28 22:16:32 +0800

    Parse Return Statements.

--- ast/ast.go
+++ ast/ast.go
@@ -33,6 +33,9 @@ type Identifier struct {
 	Value string
 }
 
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
 type LetStatement struct {
 	Token token.Token // the LET token
 	Name  *Identifier
@@ -42,5 +45,11 @@ type LetStatement struct {
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
-func (i *Identifier) expressionNode()      {}
-func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+type ReturnStatement struct {
+	Token       token.Token // the `return` token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
