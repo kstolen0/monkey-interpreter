a2b8169cbac9e9110245da650ca89382f158c058
kristian stolen <kristian@sT073N-dev.tpgi.com.au>
2024-07-24 17:06:56 +0800

    add unless macro example in test

--- evaluator/macro_expansion_test.go
+++ evaluator/macro_expansion_test.go
@@ -66,17 +66,31 @@ func TestExpandMacros(t *testing.T) {
 	}{
 		{
 			`
-let infixExpression = macro() { quote(1 + 2); };
-infixExpression();
+			let infixExpression = macro() { quote(1 + 2); };
+			infixExpression();
 `,
 			`(1 + 2)`,
 		},
 		{
 			`
-let reverse = macro(a, b) { quote(unquote(b) - unquote(a)); };
-reverse(2 + 2, 10 - 5);
+			let reverse = macro(a, b) { quote(unquote(b) - unquote(a)); };
+			reverse(2 + 2, 10 - 5);
 `,
 			`(10 - 5) - (2 + 2)`,
+		},
+		{
+			`
+			let unless = macro(condition, consequence, alternative) {
+				quote(if (!(unquote(condition))) {
+					unquote(consequence);
+				} else {
+					unquote(alternative);
+				});
+			};
+
+			unless(10 > 5, puts("not greater"), puts("greater"));
+			`,
+			`if (!(10 > 5)) { puts("not greater") } else { puts("greater") }`,
 		},
 	}
 
